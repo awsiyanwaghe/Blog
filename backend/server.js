6f9a5e2a9d161f5b22d6d71457101ea07d47a113
Awsiyan mohammad Waghe <awsiyan30@gmail.com>
2025-06-29 11:56:25 +0530

    update server.js

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ import blogRoute from "./routes/blog.route.js"
 import commentRoute from "./routes/comment.route.js"
 import cookieParser from 'cookie-parser';
 import cors from 'cors'
-import path from "path"
+// import path from "path"
 
 dotenv.config()
 const app = express()
@@ -24,17 +24,17 @@ app.use(cors({
     credentials:true
 }))
 
-const _dirname = path.resolve()
+// const _dirname = path.resolve()
 
 // apis
  app.use("/api/v1/user", userRoute)
  app.use("/api/v1/blog", blogRoute)
  app.use("/api/v1/comment", commentRoute)
 
- app.use(express.static(path.join(_dirname,"/frontend/dist")));
- app.get("*", (_, res)=>{
-    res.sendFile(path.resolve(_dirname, "frontend", "dist", "index.html"))
- });
+//  app.use(express.static(path.join(_dirname,"/frontend/dist")));
+//  app.get("*", (_, res)=>{
+//     res.sendFile(path.resolve(_dirname, "frontend", "dist", "index.html"))
+//  });
 
 app.listen(PORT, ()=>{
     console.log(`Server listen at port ${PORT}`);
